26cbaa1bb3921bba5a6c945f1dd720e42aa09626
SarahNoel <snturek@hotmail.com>
2015-07-16 09:13:38 -0600

    Added straight variables

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Cee-Lo");
+console.log("Welcome to the Daring Dice Game!");
 var die = 0;
 
 var player1 = [], computer = [];
@@ -15,70 +15,54 @@ roll(player1);
 console.log("The computer rolled " +computer);
 console.log("You rolled "+ player1);
 
-if (player1>computer) {
- console.log("You win!");
+function sum(array) {
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {
+    count = count + array[i];
+  }
+  return count;
 }
- else if (computer > player1){
+
+function winstraight(array) {
+for (var i = 0; i < array.length; i++) {
+  if (array[0] === 4 || array[1] === 4 || array[2] === 4 ){
+    if(array[0] === 5 || array[1] === 5 || array[2] === 5 ){
+      if(array[0] === 4 || array[1] === 4 || array[2] === 4 ){
+        return true;
+      }
+    }
+  }
+}
+}
+
+function losestraight(array) {
+for (var i = 0; i < array.length; i++) {
+  if (array[0] === 4 || array[1] === 4 || array[2] === 4 ){
+    if(array[0] === 5 || array[1] === 5 || array[2] === 5 ){
+      if(array[0] === 4 || array[1] === 4 || array[2] === 4 ){
+        return true;
+      }
+    }
+}
+}
+}
+
+if(winstraight(player1 )=== true){
+console.log("You win!");
+} else if (winstraight(computer)=== true){
+  console.log("The computer wins.");
+} else if (losestraight(player1)=== true) {
+  console.log("The computer wins.");
+}else if (losestraight(computer)=== true) {
+  console.log("You win!");
+}else if
+(sum(player1) > sum(computer)){
+console.log("You win!");
+}else if (sum(computer) > sum(player1)){
    console.log("The computer wins.");
  }
- else { console.log("It's a tie!")
-
+ else {
+console.log("It's a tie!");
  }
 
-// console.log("Welcome to Cee-Lo");
-//  // var numPlayers = prompt("How many Players?");
-//  var die = 0;
-//  var player1 = [], computer = [];
-//  roll(computer);
-//
-//  function roll(player) {
-//    for (var i = 0; i < 3; i++) {
-//      die = Math.ceil(Math.random()*6);
-//      player.push(die);
-//    }
-//  };
-//
-//  function compare() {
-//    if (sum(player1) > sum(computer)) {
-//      console.log('You rolled ' + player1 + ' You win!');
-//    } else {
-//      console.log('The computer rolled ' + computer + ' The computer wins :(');
-//    }
-//  };
-//
-//  function sum(array) {
-//    var count = 0;
-//    for (var i = 0; i < array.length; i++) {
-//      count = count + array[i];
-//    }
-//    return count;
-//  };
-
-
-//
-// console.log("Welcome to Cee-Lo");
-// // var numPlayers = prompt("How many Players?");
-// var die = 0;
-// var player1 = [], computer = [];
-// roll(computer);
-//
-// function roll(player) {
-//  for (var i = 0; i < 3; i++) {
-//    die = Math.ceil(Math.random()*6);
-//    player.push(die);
-//  }
-// }
-//
-// function compare() {
-//  if (sum(player1) > sum(computer)) {
-//    console.log('player1 wins!', player1);
-//  } else {
-//    console.log('computer wins :(', computer);
-//  }
-// }
-//
-// function sum(array) {
-//  var count = 0;
-//  for (var i = 0; i < array.length; i++) {
-//    count = count + array[i];
-//  }
+console.log("Refresh to play again!");
